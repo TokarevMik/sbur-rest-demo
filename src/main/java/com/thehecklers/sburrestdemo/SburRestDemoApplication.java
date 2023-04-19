16189dae2ce9b84484a18250c78d26ab8bbb5d23
TokarevMik <stiks84@gmail.com>
2023-04-19 13:54:23 +0500

    set properties 5.2

--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -1,8 +1,11 @@
 package com.thehecklers.sburrestdemo;
 
+import com.thehecklers.sburrestdemo.model.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 
@@ -10,8 +13,12 @@ import java.util.UUID;
 @ConfigurationPropertiesScan
 public class SburRestDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SburRestDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SburRestDemoApplication.class, args);
+    }
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid(){
+        return new Droid();
+    }
 }
